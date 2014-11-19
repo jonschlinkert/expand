090d0740ef12212ce32b345dfbe4dc0f45cbd574
jonschlinkert <dev@sellside.com>
2014-11-19 01:34:29 -0500

    start refactoring, update root files

---
+++ test.js
@@ -0,0 +1,18 @@
+/*!
+ * expand <https://github.com/assemble/expand>
+ *
+ * Copyright (c) 2014 , contributors.
+ * Licensed under the MIT License
+ */
+
+'use strict';
+
+var assert = require('assert');
+var should = require('should');
+var set = require('set-object');
+var expand = require('./');
+
+describe('expand', function() {
+
+
+});
